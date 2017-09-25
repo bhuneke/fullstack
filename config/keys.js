9aac5d70c42c1a3ef37ca53d7b14350a2bbf9425
Brigitte Huneke <brigittehuneke@gmail.com>
2017-09-25 13:49:47 -0500

    added mongoose

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   googleClientID: '136098394226-9v0evepad18o70jhv337jsmj1c3r2u5r.apps.googleusercontent.com',
-  googleClientSecret: 'OyNWOyIQStbSjLUUusSoMbIi'
+  googleClientSecret: 'OyNWOyIQStbSjLUUusSoMbIi',
+  mongoURI: 'mongodb://brigitte:password-emaily@ds149124.mlab.com:49124/emaily-dev-bh',
 };
