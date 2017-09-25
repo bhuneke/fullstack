65090cd75e8beb7b743f864b9d3bf740126eb35d
Brigitte Huneke <brigittehuneke@gmail.com>
2017-09-25 15:21:55 -0500

    added logout route

--- config/keys.js
+++ config/keys.js
@@ -2,4 +2,5 @@ module.exports = {
   googleClientID: '136098394226-9v0evepad18o70jhv337jsmj1c3r2u5r.apps.googleusercontent.com',
   googleClientSecret: 'OyNWOyIQStbSjLUUusSoMbIi',
   mongoURI: 'mongodb://brigitte:password-emaily@ds149124.mlab.com:49124/emaily-dev-bh',
+  cookieKey: 'lsfblksdjblisuheraaaa',
 };
